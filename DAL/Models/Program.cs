20621b8e2b7437d5dee3187538d8344bc550e679
Kalyan Mukherjee <klyn.mkrj@gmail.com>
2019-03-03 18:33:55 +0530

    EF

--- DAL/Models/Program.cs
+++ DAL/Models/Program.cs
@@ -4,9 +4,9 @@ namespace DAL
 {
     public class Program
     {
-        public static void Main(string[] args)
-        {
+        //public static void Main(string[] args)
+        //{
 
-        }
+        //}
     }
 }
